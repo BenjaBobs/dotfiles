81ccc866de1510d9b85ecf621ed588326d415da1
Benjamin <behub92@gmail.com>
2024-12-28 23:05:39 +0100

    Fix lsp hotkey groups + add formatting

---
+++ neovim/config/lua/bh/plugins/format.lua
@@ -0,0 +1,16 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+		})
+
+		vim.keymap.set("n", "<leader>bf", function()
+			conform.format({ async = true })
+		end, { desc = "[F]iles" })
+	end,
+}
