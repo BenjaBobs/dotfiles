9680c385afd632f0c18ef2bb8c1eb7ae9cd38008
Benjamin <behub92@gmail.com>
2025-02-09 09:43:04 +0100

    Format files

--- neovim/config/lua/bh/plugins/indentation.lua
+++ neovim/config/lua/bh/plugins/indentation.lua
@@ -1,26 +1,26 @@
 return {
-	"shellRaining/hlchunk.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("hlchunk").setup({
-			chunk = {
-				enable = true,
-				delay = 50,
-				duration = 50,
-				chars = {
-					horizontal_line = "─",
-					vertical_line = "│",
-					left_top = "╭",
-					left_bottom = "╰",
-					right_arrow = "─",
-				}
-			},
-			indent = {
-				enable = true,
-			},
-			line_num = {
-				enable = true,
-			}
-		})
-	end
+  "shellRaining/hlchunk.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("hlchunk").setup({
+      chunk = {
+        enable = true,
+        delay = 50,
+        duration = 50,
+        chars = {
+          horizontal_line = "─",
+          vertical_line = "│",
+          left_top = "╭",
+          left_bottom = "╰",
+          right_arrow = "─",
+        }
+      },
+      indent = {
+        enable = true,
+      },
+      line_num = {
+        enable = true,
+      }
+    })
+  end
 }
--- neovim/config/lua/bh/plugins/telescope.lua
+++ neovim/config/lua/bh/plugins/telescope.lua
@@ -1,13 +1,14 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require 'telescope.builtin'
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]iles' })
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[G]rep' })
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[H]elp' })
-		vim.keymap.set('n', '<leader>fn', function() 
-			builtin.find_files({ cwd = vim.fn.stdpath('config') })
-		end, { desc = '[N]eovim config files' })
-	end
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local builtin = require 'telescope.builtin'
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]iles' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[G]rep' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[H]elp' })
+    vim.keymap.set('n', '<leader>fn', function()
+      builtin.find_files({ cwd = vim.fn.stdpath('config') })
+    end, { desc = '[N]eovim config files' })
+  end
 }
