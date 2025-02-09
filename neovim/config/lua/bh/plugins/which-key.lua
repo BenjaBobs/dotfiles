2d02daffb3c37493ebaba80290db63f547498fe7
Benjamin <behub92@gmail.com>
2025-02-09 09:38:57 +0100

    Ex --> Oil

--- neovim/config/lua/bh/plugins/which-key.lua
+++ neovim/config/lua/bh/plugins/which-key.lua
@@ -1,37 +1,37 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
-	keys = {
-		{
-			"<leader>?",
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
 
-			function()
-				require("which-key").show({ global = false })
-			end,
-			desc = "Buffer Local Keymaps (which-key)",
-		},
-	},
-	config = function()
-		local wk = require("which-key")
-		wk.add({
-			{ "<leader>f", group = "[F]ind", icon = { icon = "🔍" } },
-			{ "<leader>b", group = "[B]uffer", icon = { icon = "" } },
-			{ "<leader>c", group = "[C]ode", icon = { icon = "" } },
-		})
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+  config = function()
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>f", group = "[F]ind", icon = { icon = "🔍" } },
+      { "<leader>b", group = "[B]uffer", icon = { icon = "" } },
+      { "<leader>c", group = "[C]ode", icon = { icon = "" } },
+    })
 
-		vim.keymap.set("n", "<leader>fb", ":Ex<CR>", { desc = "[B]rowse files" })
+    vim.keymap.set("n", "<leader>fb", ":Oil<CR>", { desc = "[B]rowse files" })
 
-		-- Move current line or selected lines up
-		vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
-		vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+    -- Move current line or selected lines up
+    vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+    vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
 
-		-- Move selected lines up or down in visual mode
-		vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-		vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-	end,
+    -- Move selected lines up or down in visual mode
+    vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+    vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+  end,
 }
