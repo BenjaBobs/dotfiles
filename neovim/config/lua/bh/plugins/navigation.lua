e220909b479e8d0764d8466281f2e2a58e518120
Benjamin <behub92@gmail.com>
2025-02-10 20:00:43 +0100

    folke/flash

---
+++ neovim/config/lua/bh/plugins/navigation.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {
+    label = {
+      rainbow = {
+        enabled = true,
+      }
+    }
+  },
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+  },
+}
