297410aa56ac95332d3d8ca57fac1bee34c77d58
Benjamin <behub92@gmail.com>
2025-06-27 23:47:17 +0200

    Display whitespace in vim

--- neovim/config/lua/bh/vim-vars.lua
+++ neovim/config/lua/bh/vim-vars.lua
@@ -38,3 +38,23 @@ vim.diagnostic.config({
     float = true,
   },
 })
+
+-- Display whitespace
+-- Nerd Font characters that render well in almost every terminal
+local dots = "⋅" -- U+22C5  (middle dot)   for normal spaces
+local trail = "•" -- U+2022  (bullet)       for *trailing* spaces
+local tab_in = "▸" -- U+25B8  (small arrow)  for the first cell of a tab
+local tab_fill = " " -- a plain space for the filler cells
+vim.opt.list = true
+vim.opt.listchars = {
+  space = dots,
+  trail = trail,
+  tab = tab_in .. tab_fill,
+  extends = "›", -- overflow to the right
+  precedes = "‹", -- overflow to the left
+  nbsp = "␣", -- non-breaking space
+}
+vim.cmd([[
+  highlight! link NonText Whitespace        " keep them the same colour
+  highlight! Whitespace gui=nocombine guifg=#5c6370 ctermfg=240
+]])
