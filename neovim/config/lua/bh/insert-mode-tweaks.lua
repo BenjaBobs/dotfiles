196164dfb225449e8d9dbd0e94936d0550f2ffb9
Benjamin <behub92@gmail.com>
2025-08-28 19:58:46 +0200

    Ctrl delete keys

--- neovim/config/lua/bh/insert-mode-tweaks.lua
+++ neovim/config/lua/bh/insert-mode-tweaks.lua
@@ -1,3 +1,7 @@
 -- nvim already has CTRL + w for delete word in backwards direction
 -- so this adds CTRL + e for delete word in forward direction
 vim.keymap.set("i", "<C-e>", "<C-g>u<C-o>de", { noremap = true, silent = true })
+
+-- VSCode-like word deletion
+vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true, silent = true }) -- Delete word backwards
+vim.keymap.set("i", "<C-Del>", "<C-g>u<C-o>de", { noremap = true, silent = true }) -- Delete word forwards
