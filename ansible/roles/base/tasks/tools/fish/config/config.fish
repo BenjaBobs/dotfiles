1fa9a1c71266f129021b529fbc6da7b228cfddc4
Benjamin Hubert <behub92@gmail.com>
2026-03-15 12:12:21 +0100

    Add support for claude profiles

--- ansible/roles/base/tasks/tools/fish/config/config.fish
+++ ansible/roles/base/tasks/tools/fish/config/config.fish
@@ -10,3 +10,23 @@ set EDITOR nvim
 if test -f ~/.config/fish/local_config.fish
     source ~/.config/fish/local_config.fish
 end
+
+if command -q claude
+    if command -q nono
+        function claude-work
+            mkdir -p ~/.claude-work
+            CLAUDE_CONFIG_DIR=~/.claude-work nono run -v --profile claude-flex --allow-cwd --allow ~/.claude-work -- claude $argv
+        end
+        function claude-home
+            mkdir -p ~/.claude-home
+            CLAUDE_CONFIG_DIR=~/.claude-home nono run -v --profile claude-flex --allow-cwd --allow ~/.claude-home -- claude $argv
+        end
+    else
+        function claude-work
+            echo "Don't use claude-code without nono"
+        end
+        function claude-home
+            echo "Don't use claude-code without nono"
+        end
+    end
+end
