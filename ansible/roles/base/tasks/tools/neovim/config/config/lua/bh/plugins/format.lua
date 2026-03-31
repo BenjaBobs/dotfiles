2cbca00980a667971816405106bfdd7f7610914f
Benjamin Hubert <behub92@gmail.com>
2026-03-31 19:45:17 +0200

    Only use biome

--- ansible/roles/base/tasks/tools/neovim/config/config/lua/bh/plugins/format.lua
+++ ansible/roles/base/tasks/tools/neovim/config/config/lua/bh/plugins/format.lua
@@ -5,14 +5,14 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        javascript = { "biome", "eslint_d", "prettier" },
-        javascriptreact = { "biome", "eslint_d", "prettier" },
-        typescript = { "biome", "eslint_d", "prettier" },
-        typescriptreact = { "biome", "eslint_d", "prettier" },
-        css = { "biome", "prettier" },
-        scss = { "biome", "prettier" },
-        html = { "biome", "prettier" },
-        json = { "biome", "prettier" },
+        javascript = { "biome" },
+        javascriptreact = { "biome" },
+        typescript = { "biome" },
+        typescriptreact = { "biome" },
+        css = { "biome" },
+        scss = { "biome" },
+        html = { "biome" },
+        json = { "biome" },
         yaml = { "prettier" },
         markdown = { "prettier" },
         cs = { "csharpier" },
